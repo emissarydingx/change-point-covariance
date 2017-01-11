217c2c28b881a6f6d1b196238b49a09b46daef53
Xin Ding <Xin Ding>
2017-01-11 08:23:09 -0600

    First commit

---
+++ change_point_bootstrap.R
@@ -0,0 +1,64 @@
+rm(list=ls())
+library(MASS)
+library(doParallel)
+library(foreach)
+library(mvtnorm)
+library(ContaminatedMixt)
+# library(distrEllipse)
+
+# setwd("C:\\Users\\Xin\\OneDrive\\research\\change point covariance\\code\\simulation")
+# setwd('/home/xin/OneDrive/Research/change point covariance/code/simulation')
+# setwd("/home/xin/Dropbox/Research/change point covariance/code/simulation")
+setwd("C:\\Users\\Xin\\Dropbox\\Research\\change point covariance\\code\\simulation")
+source("change_point_bootstrap_lib.R")
+# -------------------------------------------
+# Simulation setup
+# -------------------------------------------
+ncores=8
+n=100
+d=40
+nBoot=100
+
+#bandwidth fitting
+# b=0.8*n^(-1/5)
+b=0.1
+
+Sigma1=diag(rep(1,d))
+Sigma2=diag(rep(1,d))
+
+cp_indx=50#the index of the change point
+
+#data generation
+para=list(Sigma1=Sigma1,Sigma2=Sigma2,
+          df=10)
+X=bootstrap_gen(n,d,cp_indx,family='t',para=para)
+
+#bootstrap
+W=bootstrap_fit(X,nBoot,b,ncores)
+W_sort=sort(W)
+
+#Empirical cdf of W
+ecdf_W=ecdf(W)
+prob_W=ecdf_W(W_sort)#discontinuity points of W's ecdf
+prob_W_interval=c(0,prob_W[-length(prob_W)])
+
+alpha=seq(0.01,0.99,length.out=25)#range of probability
+
+#sample quantile (prob 0 represents smallest sample while prob 1 represents largest sample)
+sq=sapply(1:length(alpha),FUN=function(x){
+  W_sort[sum(alpha[x]>prob_W_interval)]
+})
+# sq=quantile(W, probs = alpha)
+alpha_hat=sapply(1:length(sq),FUN=function(x){
+  mean(W_sort<=sq[x])
+})
+
+plot(alpha,alpha_hat,xlab='alpha',ylab='Bootstrap approximation',type='b')
+abline(a=0,b=1)
+
+# save.image("change_point_detection.Rdata")
+
+
+
+
+
---
+++ change_point_bootstrap_lib.R
@@ -0,0 +1,94 @@
+library(MASS)
+library(compiler)
+library(doParallel)
+library(foreach)
+library(parallel)
+library(mvtnorm)
+library(ContaminatedMixt)
+# library(distrEllipse)
+
+# #######################################
+# 1. bootstrap_gen
+# n: n samples
+# d: d dimensions
+# nBoot: a number represents the number of sampling
+# cp_indx: the change point index (after that the covariance matrices begins changing)
+# Sigmas: a list that contains covariance matrix of data
+bootstrap_gen<-function(n,d,cp_indx,Sigma1,Sigma2,family,para){
+  Sigma1=para$Sigma1
+  Sigma2=para$Sigma2
+  #normal distribution
+  if (family=='normal'){
+    X1=mvrnorm(n = cp_indx, mu=rep(0,d), Sigma=Sigma1)
+    X2=mvrnorm(n = n-cp_indx, mu=rep(0,d), Sigma=Sigma2)
+  }
+  #t distribution
+  if (family=='t'){
+    X1=rmvt(cp_indx, sigma = Sigma1, df = para$df)
+    X2=rmvt(n-cp_indx, sigma = Sigma2, df = para$df)
+  }
+  # mixed distribution
+  if (family=='mix'){
+    X1=rCN(n = cp_indx, mu = rep(0,d), Sigma1, alpha = 0.8, eta = 2.51)
+    X2=rCN(n = n-cp_indx, mu = rep(0,d), Sigma2, alpha = 0.8, eta = 2.51)
+  }
+  #data matrix
+  X=rbind(X1,X2)
+  return(X)
+}
+
+
+
+
+# #######################################
+# 2. bootstrap_fit
+# X: data matrix (n by p)
+# it: it th observation
+# nBoot: a number represents the number of sampling
+# b: bandwidth
+# ncores: cpu number
+dev_Sig_at_t<-function(it,X_star,e,b){
+  n=dim(X_star)[1]
+  d=dim(X_star)[2]
+  hnb=round((b*n)/2)#half bandwidth
+  s1=crossprod(X_star[((it-hnb):it),]*drop(e[((it-hnb):it)]),X_star[((it-hnb):it),])
+  s2=crossprod(X_star[((it+1):(it+hnb)),]*drop(e[((it+1):(it+hnb))]),X_star[((it+1):(it+hnb)),])
+  dev=max(abs(s1-s2))/hnb
+  return(dev)
+}
+
+bootstrap_fit<-function(X,nBoot,b,ncores){
+  n=dim(X)[1]
+  d=dim(X)[2]
+  hnb=round((b*n)/2)#half bandwidth
+  #generate iid e_i: nBoot_e n by nBoot
+  # nBoot_e=mvrnorm(n = n, mu=rep(0,nBoot), Sigma=diag(rep(1,nBoot)))
+  nBoot_e=sapply(1:nBoot,FUN=function(x){
+    rnorm(n, mean = 0, sd = 1)
+  })
+  #bootstraping
+  # X_bootstrap: n*d*nBoot
+  X_bootstrap=sapply(1:nBoot,FUN=function(x){
+    indx_tmp=sample(1:n,replace=TRUE)
+    return(X[indx_tmp,])
+  },simplify="array")  
+  cl <- makeCluster(ncores)
+  registerDoParallel(cl)
+  tmp<-foreach(step=1:nBoot, .combine='cbind',.multicombine=TRUE,.export=c('dev_Sig_at_t'))%:%
+    foreach(it=(hnb+1):(n-hnb), .combine='c',.export=c('dev_Sig_at_t'))%dopar%{
+      X_star=X_bootstrap[,,step]
+      e=nBoot_e[,step]
+      result=dev_Sig_at_t(it,X_star,e,b)
+    }
+  stopCluster(cl)
+  W_all=as.vector(apply(tmp,MARGIN=2,max))
+  return(W_all)
+}
+
+
+
+
+
+
+
+
